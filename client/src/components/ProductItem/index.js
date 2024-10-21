d1b1306f5a5f16747e1400f5b282655073f2f2a0
Oliver Lopez <oliverberto@gmail.com>
2024-10-21 15:31:14 -0400

    refactored the productItem component to useDispatch to dispatch Redux actions and useSelector to access cart from the Redux store

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { pluralize } from "../../utils/helpers";
+import { useDispatch, useSelector } from "react-redux";
+// Import redux actions
+import { addToCart, updateCartQuantity } from "../../redux/slices/cartSlice";
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
+  // Use dispatch from Redux 
+  const dispatch = useDispatch();
+  // Access cart items from Redux store
+  const  cart  = useSelector(state => state.cart.cart);
 
   const {
     image,
@@ -16,28 +20,31 @@ function ProductItem(item) {
     quantity
   } = item;
 
-  const { cart } = state
 
-  const addToCart = () => {
+
+  const addProductToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    
     if (itemInCart) {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
+      // Dispatch the action to update the cart quantity
+      dispatch(updateCartQuantity({
         _id: _id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-      });
+      }));
+
       idbPromise('cart', 'put', {
         ...itemInCart,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
     } else {
-      dispatch({
-        type: ADD_TO_CART,
-        product: { ...item, purchaseQuantity: 1 }
-      });
+      dispatch(addToCart({ 
+        ...item,
+        purchaseQuantity: 1 
+      }));
+        
       idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
     }
-  }
+  };
 
   return (
     <div className="card px-1 py-1">
@@ -52,7 +59,7 @@ function ProductItem(item) {
         <div>{quantity} {pluralize("item", quantity)} in stock</div>
         <span>${price}</span>
       </div>
-      <button onClick={addToCart}>Add to cart</button>
+      <button onClick={addProductToCart}>Add to cart</button>
     </div>
   );
 }
